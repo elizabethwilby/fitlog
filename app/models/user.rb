75ca1a13679593235bc601e275b38d7b6b533554
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-09-01 10:10:04 -0400

    Added usrname and passord complexity validations, cleaned up devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   has_many :completed_workouts, through: :workout_logs, source: :workout
   has_many :workout_exercises, through: :workouts
   has_many :exercises, through: :workout_exercises
+
+  validates :username, presence: true
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    unless password.match?(/\A(?=.*[A-Z])(?=.*\d).+\z/)
+      errors.add(:password, "must include at least one capital letter and number. Must be 6 characters.")
+    end
+  end
 end
